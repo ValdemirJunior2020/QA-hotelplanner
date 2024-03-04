91a2f31ec2499691d0ca702a9a9d6800cc7a44d5
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-03-04 14:07:00 -0500

    Update script.js

--- Links/script.js
+++ Links/script.js
@@ -8,15 +8,15 @@ function checkPassword() {
 
         // Add your links dynamically here, with styling for spacing and target="_blank"
         document.getElementById('links').innerHTML += `
-            <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" style="margin-bottom: 10px; display: block;" target="_blank">Old Responses</a>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" style="margin-bottom: 10px; display: block;" target="_blank">Form 2024</a>
+           
+            <a href="https://docs.google.com/spreadsheets/d/1teUVnlSQB4PcpCEukvh20Pefu8aWtMrw5nnfGagSTrM/edit#gid=831364116" style="margin-bottom: 10px; display: block;" target="_blank">CONCENTRIX</a>
+            <a href="https://docs.google.com/spreadsheets/d/1teUVnlSQB4PcpCEukvh20Pefu8aWtMrw5nnfGagSTrM/edit#gid=1603698297" style="margin-bottom: 10px; display: block;" target="_blank">BUWELO</a>
+            <a href="https://docs.google.com/spreadsheets/d/1teUVnlSQB4PcpCEukvh20Pefu8aWtMrw5nnfGagSTrM/edit#gid=392631628" style="margin-bottom: 10px; display: block;" target="_blank">BUWELO Phi.</a>
+            <a href="https://docs.google.com/spreadsheets/d/1teUVnlSQB4PcpCEukvh20Pefu8aWtMrw5nnfGagSTrM/edit#gid=1980953920" style="margin-bottom: 10px; display: block;" target="_blank">WNS</a>
+            <a href="https://docs.google.com/spreadsheets/d/1teUVnlSQB4PcpCEukvh20Pefu8aWtMrw5nnfGagSTrM/edit#gid=982878336" style="margin-bottom: 10px; display: block;" target="_blank">Teleperformance</a>
             <a href="https://drive.google.com/file/d/1yUR_25xjqSukP9nVj0hvsKX0pYMMkaZi/view?usp=sharing" style="margin-bottom: 10px; display: block;" target="_blank">Training Guide</a>
-            <a href="https://docs.google.com/spreadsheets/d/1XgZTnQ8nd7V_9tWeRQxE7Yx8B8mzc0GtsciOTm5xhDQ/edit#gid=60603194" style="margin-bottom: 10px; display: block;" target="_blank">ASAP</a>
-            <a href="https://docs.google.com/spreadsheets/d/1teUVnlSQB4PcpCEukvh20Pefu8aWtMrw5nnfGagSTrM/edit?resourcekey#gid=1147589046" style="margin-bottom: 10px; display: block;" target="_blank">New Responses 2024</a>
-            <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" style="margin-bottom: 10px; display: block;" target="_blank">HP MBR Jim's Link</a>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe6Zo2XXP-otpbMzScXIwSqv3qFYBmsl2OS7Swo389ZpzCG5w/viewform" style="margin-bottom: 10px; display: block;" target="_blank">2023 Form</a>
-            <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" style="margin-bottom: 10px; display: block;" target="_blank">2023 Answers</a>
-            <a href="https://q-a-notes.vercel.app/" style="margin-bottom: 10px; display: block;" target="_blank">Dropdown-discriptions 2024</a>
+           
+            <a href="slack://open" style="margin-bottom: 10px; display: block;" target="_blank">Open Slack</a>
             
         `;
     } else {
